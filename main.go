81a739074bcf0e19d2b2a390d70bb57cbaef91b3
Josh Elias <josh@daemontech.tools>
2024-06-12 15:06:35 -0400

    feat: list xbps repos and clean app dir

--- main.go
+++ main.go
@@ -1,14 +1,46 @@
 package main
 
 import (
-	// "bufio"
+	"errors"
 	"fmt"
-	// "log"
+	"github.com/spf13/viper"
 	"os"
-	// "path/filepath"
+	"path/filepath"
 )
 
 func main() {
+	appPath, err := getAppPath()
+	if err != nil {
+		return
+	}
+	repoUrls, err := getXbpsRepos()
+	if err != nil {
+		return
+	}
+
+	for _, url := range repoUrls {
+		fmt.Println(url)
+	}
+
+	appDir := filepath.Join(appPath, "void-repos")
+	err = cleanDir(appDir)
+	if err != nil {
+		return
+	}
+}
+
+func getAppPath() (path string, err error) {
+	viper.AutomaticEnv()
+	path = viper.GetString("HOME")
+	if path == "" {
+		err = errors.New("HOME is not defined")
+		return
+	}
+	fmt.Println("App path:", path)
+	return
+}
+
+func getXbpsRepos() (repoUrls []string, err error) {
 	cwd, err := os.Getwd()
 	if err != nil {
 		fmt.Println("Error getting cwd:", err)
@@ -16,6 +48,30 @@ func main() {
 	}
 	fmt.Println("CWD:", cwd)
 
-	// f, err := os.Open('
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(cwd)
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Unable to read config")
+	}
 
+	repoUrls = viper.GetStringSlice("repoUrls")
+	if repoUrls == nil {
+		fmt.Print("Please provide repo urls")
+	}
+	fmt.Println("repoUrls:", repoUrls)
+	return
+}
+
+func cleanDir(path string) (err error) {
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		fmt.Println("App dir does not exist")
+		return
+	}
+	err = os.RemoveAll(path)
+	if err != nil {
+		fmt.Println("Error deleting app dir:", err)
+	}
+	return
 }
